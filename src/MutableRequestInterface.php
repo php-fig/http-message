4ee0416e00ba86d74126c723fb53550228285de8
kiler129 <grzegorz@noflash.pl>
2015-07-11 15:19:35 +0200

    Added mutable methods for RequestInterface.

---
+++ src/MutableRequestInterface.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Psr\Http\Message;
+
+/**
+ * This interface extends standard RequestInterface by adding mutability.
+ */
+interface MutableRequestInterface extends RequestInterface
+{
+    /**
+     * Modifies current instance by setting specified request-target.
+     *
+     * If the request needs a non-origin-form request-target — e.g., for
+     * specifying an absolute-form, authority-form, or asterisk-form —
+     * this method may be used to create an instance with the specified
+     * request-target, verbatim.
+     *
+     * This method MUST be implemented in such a way as to modify current
+     * instance of object, and MUST return the same instance that has been called.
+     *
+     * @link http://tools.ietf.org/html/rfc7230#section-2.7 (for the various
+     *     request-target forms allowed in request messages)
+     * @param mixed $requestTarget
+     * @return self
+     */
+    public function setRequestTarget($requestTarget);
+
+    /**
+     * Modifies current instance by setting specified HTTP method.
+     *
+     * While HTTP method names are typically all uppercase characters, HTTP
+     * method names are case-sensitive and thus implementations SHOULD NOT
+     * modify the given string.
+     *
+     * This method MUST be implemented in such a way as to modify current
+     * instance of object, and MUST return the same instance that has been called.
+     *
+     * @param string $method Case-sensitive method.
+     * @return self
+     * @throws \InvalidArgumentException for invalid HTTP methods.
+     */
+    public function setMethod($method);
+
+    /**
+     * Modifies current instance by setting specified URI.
+     *
+     * This method MUST update the Host header of the returned request by
+     * default if the URI contains a host component. If the URI does not
+     * contain a host component, any pre-existing Host header MUST be carried
+     * over to the returned request.
+     *
+     * You can opt-in to preserving the original state of the Host header by
+     * setting `$preserveHost` to `true`. When `$preserveHost` is set to
+     * `true`, this method interacts with the Host header in the following ways:
+     *
+     * - If the the Host header is missing or empty, and the new URI contains
+     *   a host component, this method MUST update the Host header in the returned
+     *   request.
+     * - If the Host header is missing or empty, and the new URI does not contain a
+     *   host component, this method MUST NOT update the Host header in the returned
+     *   request.
+     * - If a Host header is present and non-empty, this method MUST NOT update
+     *   the Host header in the returned request.
+     *
+     * This method MUST be implemented in such a way as to modify current
+     * instance of object, and MUST return the same instance that has been called.
+     *
+     * @link http://tools.ietf.org/html/rfc3986#section-4.3
+     * @param UriInterface $uri New request URI to use.
+     * @param bool $preserveHost Preserve the original state of the Host header.
+     * @return self
+     */
+    public function setUri(UriInterface $uri, $preserveHost = false);
+}
